df8946eb8fe78e8d082c1b70afc31e966f974dfb
baqtrip <106026177+baqtrip@users.noreply.github.com>
2022-11-20 23:49:04 +0300

    added popup verst

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Привет ребятки')
+const editButton = profile.querySelector('.profile__edit');
+
+function openPopup(popup) {
+  popup.classList.add('popup_animated');
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
