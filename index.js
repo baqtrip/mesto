0882d310710de50ebd73f42ac720727df0a3827b
baqtrip <106026177+baqtrip@users.noreply.github.com>
2022-11-23 18:14:13 +0300

    half edded js

--- index.js
+++ index.js
@@ -38,4 +38,31 @@ popupCloseButtonElement.addEventListener('click', closePopup);
 
 
 
+// Находим форму в DOM
+let formElement = document.querySelector('.form'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = form.querySelector('.profile__title'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = form.querySelector('.profile__subtitle'); // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    profile__title.textContent = nameInput.value // Получите значение полей jobInput и nameInput из свойства value
+
+    profile__title.textContent = jobInput.value // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+
+
 
