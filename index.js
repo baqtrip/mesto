1f16191858cc93ba2b53f1cdcf89417e07aacdca
baqtrip <106026177+baqtrip@users.noreply.github.com>
2022-11-26 00:18:17 +0300

    make mobile popup

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ const popupOpenButtonElement = document.querySelector('.profile__edit');
 const nameInput = document.querySelector('.profile__title');
 const jobInput = document.querySelector('.profile__subtitle');
 
-
-let formElement = document.querySelector('.form'); 
-
+let formElement = document.querySelector('.form');
+// let popupSave = formElement.querySelector('.popup__save');
 let profileName = formElement.querySelector('#input-name'); 
 let profileJob = formElement.querySelector('#input-job'); 
 
 const openPopup = function () {
   popupElement.classList.add('popup_is-opened');
-  profileName.value = nameInput.textContent; //при открытии попапа текст берется из профиля?
+  profileName.value = nameInput.textContent; 
   profileJob.value = jobInput.textContent;
 }
 
 const closePopup = function() {
   popupElement.classList.remove('popup_is-opened');
-
+  // popupSave.classList.remove('popup_is-opened');
 }
- popupOpenButtonElement.addEventListener('click', openPopup);
- popupCloseButtonElement.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.  
   nameInput.textContent = profileName.value;   
   jobInput.textContent = profileJob.value;
+
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', closePopup); 
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
